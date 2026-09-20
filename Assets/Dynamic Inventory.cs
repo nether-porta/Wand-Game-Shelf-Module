f363f207126e3dcec0f4b35fb261b4b11e553a06
nether-porta <mibsamaarij09+github@gmail.com>
2026-09-20 15:02:20 +0500

    Removed Troublesome FIles

--- Assets/Dynamic Inventory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DynamicInventory : ScriptableObject
-{
-    public int maxItems = 10;
-    public List<WandBoxInstance>
-}
